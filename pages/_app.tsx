4d148fc8858451530e815ae5e3ef5744a91fe6b6
sobhanbera <sobhanbera258@gmail.com>
2023-01-12 00:40:14 +0530

    almost done with dev setup and file structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.scss'
+import type {AppProps} from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import {ThemeProvider} from '@/hooks'
+import {ErrorBoundary} from '@/error'
+
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <ErrorBoundary id={'theme'}>
+            <ThemeProvider>
+                <ErrorBoundary id={'app'}>
+                    <Component {...pageProps} />
+                </ErrorBoundary>
+            </ThemeProvider>
+        </ErrorBoundary>
+    )
 }
